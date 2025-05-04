3a2667c5d4f9ff7f7208362d7c45bb6f586cdee7
Arly Moura <arlymoura@gmail.com>
2025-05-04 14:35:59 -0300

    feat: Add trait for simulation factory to include result details

--- spec/factories/simulations.rb
+++ spec/factories/simulations.rb
@@ -6,5 +6,18 @@ FactoryBot.define do
     birth_date { 30.years.ago.to_date }
     result { nil }
     status { :pending }
+
+    trait :with_result do
+      result do
+        {
+          total_paid: 10272.84,
+          total_interest: 272.84,
+          payment_per_month: 856.07,
+          annual_interest_rate: 5.0,
+        }
+      end
+    end
   end
+
+
 end
